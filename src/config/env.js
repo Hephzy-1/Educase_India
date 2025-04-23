ad351fa369ba850224ac49104714f9b2f900552a
Hephzy-1 <hzdelight01@gmail.com>
2025-04-23 22:37:10 +0100

    fix: deployment configuration error

--- src/config/env.js
+++ src/config/env.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const env = {
   PORT: process.env.PORT || 3000,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_PASSWORD: process.env.DB_PASS,
+  DB_NAME: process.env.DB_NAME || process.env.MYSQL_DATABASE,
+  DB_USER: process.env.DB_USER || process.env.MYSQLUSER,
+  DB_HOST: process.env.DB_HOST || process.env.MYSQLHOST,
+  DB_PASSWORD: process.env.DB_PASS || process.env.MYSQLPASSWORD,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
 }
